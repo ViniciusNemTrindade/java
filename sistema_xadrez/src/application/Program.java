a885f5ae3204fb0496929ce37511171ecb113ba2
Vinícius Trindade <vinicius_rx@hotmail.com>
2019-07-25 18:58:40 -0300

    Primeira classe: Posicao

---
+++ sistema_xadrez/src/application/Program.java
@@ -0,0 +1,15 @@
+// @author Vinícius Trindade
+package application;
+
+import tabuleiro.Posicao;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Posicao pos = new Posicao(3, 5);
+        
+        System.out.println(pos);
+    }
+    
+}
