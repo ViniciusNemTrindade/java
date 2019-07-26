1f92d285c0b8cf4b30a46d06783ec02d028c50a0
Vinícius Trindade <vinicius_rx@hotmail.com>
2019-07-25 23:08:10 -0300

    TabuleiroException e programação defensiva

--- sistema_xadrez/src/tabuleirodojogo/Tabuleiro.java
+++ sistema_xadrez/src/tabuleirodojogo/Tabuleiro.java
@@ -2,11 +2,15 @@
 package tabuleirodojogo;
 
 public class Tabuleiro {
+
     private int linhas;
     private int colunas;
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
+        if (linhas < 1 || colunas < 1) {
+            throw new TabuleiroException("Erro ao criar o tabuleiro: precisa existir pelo menos uma linha e uma coluna");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
@@ -16,28 +20,44 @@ public class Tabuleiro {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-    
-    public Peca peca(int linha, int coluna){
+    public Peca peca(int linha, int coluna) {
+        if (!posicaoExiste(linha, coluna)) {
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
         return pecas[linha][coluna];
     }
-    
-    public Peca peca(Posicao posicao){
+
+    public Peca peca(Posicao posicao) {
+        if (!posicaoExiste(posicao)) {
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
         return pecas[posicao.getLinha()][posicao.getColuna()];
     }
-    
+
     public void alocarPeca(Peca peca, Posicao posicao) {
+        if (temUmaPosicao(posicao)) {
+            throw new TabuleiroException("Ja existe no tabuleiro, uma peca na posicao " + posicao);
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = peca;
-        peca.posicao = posicao; 
+        peca.posicao = posicao;
+    }
+
+    private boolean posicaoExiste(int linha, int coluna) {
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+
+    public boolean posicaoExiste(Posicao posicao) {
+        return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+    }
+
+    public boolean temUmaPosicao(Posicao posicao) {
+        if (!posicaoExiste(posicao)) {
+            throw new TabuleiroException("Posicao não existe no tabuleiro");
+        }
+        return peca(posicao) != null;
     }
 }
---
+++ sistema_xadrez/src/tabuleirodojogo/TabuleiroException.java
@@ -0,0 +1,9 @@
+// @author Vinícius Trindade
+package tabuleirodojogo;
+
+public class TabuleiroException extends RuntimeException {
+    
+    public TabuleiroException(String msg) {
+        super(msg);
+    }
+}
